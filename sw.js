e98ca944294a13f621614addb8bea6669a81e986
dtuankun <dtuankun@ucsd.edu>
2021-11-05 12:57:10 +0700

    feat: use normal cache

--- sw.js
+++ sw.js
@@ -39,9 +39,7 @@ self.addEventListener('install', function (event) {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       console.log('Opened cache');
-      return cache.addAll(
-        urlsToCache.map((url) => new Request(url, { mode: 'no-cors' }))
-      );
+      return cache.addAll(urlsToCache);
     })
   );
 });
