1b92e9aa61ccaeb11c1d60d05b6b26072c972673
dtuankun <dtuankun@ucsd.edu>
2021-11-05 11:48:22 +0700

    feat: no-cors for all link

--- sw.js
+++ sw.js
@@ -22,9 +22,6 @@ const urlsToCache = [
   'https://introweb.tech/assets/json/stuffing.json',
   'https://introweb.tech/assets/json/turkey.json',
   'https://introweb.tech/assets/json/pumpkinPie.json',
-];
-
-const urlsToPrefetch = [
   'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-190807-ghost-cookies-0031-landscape-pf-1566483952.jpg',
   'https://www.recipetineats.com/wp-content/uploads/2020/08/My-best-Vanilla-Cake_9.jpg',
   'https://joyfoodsunshine.com/wp-content/uploads/2018/02/best-chocolate-chip-cookies-recipe-1.jpg',
@@ -42,10 +39,9 @@ self.addEventListener('install', function (event) {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       console.log('Opened cache');
-      cache.addAll(
-        urlsToPrefetch.map((url) => new Request(url, { mode: 'no-cors' }))
+      return cache.addAll(
+        urlsToCache.map((url) => new Request(url, { mode: 'no-cors' }))
       );
-      return cache.addAll(urlsToCache);
     })
   );
 });
